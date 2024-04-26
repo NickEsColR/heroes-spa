f2760a4c0e95bb6e5bae782450eba619e0577a21
NickEsColR <nicoesol0818@gmail.com>
2024-04-26 13:35:55 -0500

    feat:keep user logged

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,26 +3,38 @@ import { authReducer } from "./authReducer";
 import { AuthContext } from "./AuthContext";
 import { types } from "../types";
 
+const init = () => {
+    return JSON.parse(localStorage.getItem("user")) || { logged: false };
+};
 
 export const AuthProvider = ({ children }) => {
+    const [authState, dispatch] = useReducer(
+        authReducer,
+        {
+            logged: false,
+        },
+        init
+    );
 
-    const [authState,dispatch] = useReducer(authReducer, {
-        logged:false,
-    })
-
-    const login = (name = '') => {
+    const login = (name = "") => {
+        const user = { name };
         const action = {
             type: types.login,
-            payload: {
-                name
-            }
-        }
+            payload: user,
+        };
+        localStorage.setItem("user", JSON.stringify(user));
 
         dispatch(action);
-    }
+    };
 
-    return <AuthContext.Provider value={{
-        ...authState,
-        login
-    }}>{children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider
+            value={{
+                ...authState,
+                login,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
