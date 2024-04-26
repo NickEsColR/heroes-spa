f447ee7a8588b4a807b7e79789442d6697761e41
NickEsColR <nicoesol0818@gmail.com>
2024-04-25 21:08:19 -0500

    use 2 routers AppRouter and HeroesRoutes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DCPage } from "../heroes/pages/DCPage";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { Navbar } from "../ui";
+import { Route, Routes } from "react-router-dom";
+
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
 
 export const AppRouter = () => {
     return (
         <>
-
-            <Navbar />
-            
             <Routes>
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DCPage />} />
                 <Route path="login" element={<LoginPage />} />
 
-                <Route path="/" element={<Navigate to="/marvel" />} />
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
         </>
     );
